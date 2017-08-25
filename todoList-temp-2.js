4b9d745bf562103c24a9a1e84733e580bb63d280
Ivica Markovic <vitz01@gmail.com>
2017-08-25 18:35:40 +1000

    Updated with directional arrows to move todolist around

--- todoList-temp-2.js
+++ todoList-temp-2.js
@@ -63,54 +63,31 @@ var todoList = {
   },
   movePosition: function (position, direction) {
     var store = JSON.parse(localStorage.getItem("todos-local"));
-    if (direction === "up" && parseInt(position) === 0) {
-      var newTodos = store.map(function(item) {
-        if (item.position === 0) {
-          item.position = store.length -1;
-          return item;
-        }else {
-          item.position -= 1;
-          return item;
+    var newTodos = store.map(function(item) {
+        if (direction === "up") {
+          if (item.position === 0) {
+            item.position = store.length -1;
+            return item;
+          }else if (parseInt(position) === 0) {
+            item.position -= 1;
+            return item;
+          }else if (item.position === (parseInt(position) -1)) {
+            item.position += 1;
+            return item;
+          }else if (item.position === parseInt(position)) {
+            item.position -= 1;
+            return item;
+          }else {
+            return item;
+          }
+        }else if (direction === "down") {
+
         }
-      });
-    }else if (direction === "up"){
-      var newTodos = store.map(function(item) {
-        if (item.position === (parseInt(position) -1)) {
-          item.position += 1;
-          return item;
-        }else if (item.position === parseInt(position)) {
-          item.position -= 1;
-          return item;
-        }else {
-          return item;
-        }
-      });
-    }else if (direction === "down" && parseInt(position) === store.length -1) {
-      var newTodos = store.map(function(item) {
-        if (item.position === store.length -1){
-          item.position = 0;
-          return item;
-        }else {
-          item.position += 1;
-          return item;
-        }
-      });
-    }else if (direction === "down") {
-      var newTodos = store.map(function(item) {
-        if (item.position === parseInt(position)) {
-          item.position += 1;
-          return item;
-        }else if (item.position === parseInt(position) +1) {
-          item.position -= 1;
-          return item;
-        }else {
-          return item;
-        }
-      });
-    }
+    });
     newTodos.sort(function (a, b) {
       return a.position - b.position;
     });
+
     this.todos = newTodos;
     view.displayTodos();
   },
